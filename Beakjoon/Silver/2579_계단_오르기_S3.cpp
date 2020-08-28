f30eea597c3540d18fa2df5b426595d013762bf3
kimdy003 <kyl123451@naver.com>
2020-08-28 22:25:03 +0900

    Create 2579_계단_오르기_S3.cpp

---
+++ Beakjoon/Silver/2579_계단_오르기_S3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int step[301] = {0};
+int dp[301] = {0};
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        cin >> step[i];
+    }
+
+    dp[1] = step[1];
+    dp[2] = step[1] + step[2];
+    for(int i=3; i<=n; i++){
+        dp[i] = max(dp[i-3]+step[i-1]+step[i], dp[i-2]+step[i]);
+    }
+    cout << dp[n];
+    
+}
