7922e475904326dd24a38d2dfcce50b635b5acc4
kimdy003 <kyl123451@naver.com>
2020-09-13 22:06:15 +0900

    Create 12865_평범한_배낭.cpp

---
+++ Beakjoon/Gold/12865_평범한_배낭.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int dp[101][100001] = {0};
+int w[101] = {0};
+int v[101] = {0};
+
+int main(){
+    int n, k;
+    cin >>  n >> k;
+
+    for(int i=1; i<=n; i++){
+        cin >> w[i] >> v[i];
+    }
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=k; j++){
+            dp[i][j] = dp[i-1][j];
+            if(w[i] <= j){
+                dp[i][j] = max(dp[i][j], dp[i-1][j-w[i]] + v[i]);
+            }
+        }
+    }
+
+    cout << dp[n][k];
+}
