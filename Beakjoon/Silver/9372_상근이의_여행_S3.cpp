e7c1e0f952c7232fdb8a3f9f94e86b55619a2d42
kimdy003 <kyl123451@naver.com>
2020-10-03 13:38:39 +0900

    Create 9372_상근이의_여행_S3.cpp

---
+++ Beakjoon/Silver/9372_상근이의_여행_S3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        cin >> n >> m;
+        
+        for(int i=0; i<m; i++){
+            int x, y;
+            cin >> x >> y;
+        }
+        
+        cout << n-1 << "\n";
+    }
+}
