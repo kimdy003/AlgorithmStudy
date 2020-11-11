609bc9b0d058422904f365f4914ab2c8384e6d69
kimdy003 <kyl123451@naver.com>
2020-11-11 19:48:42 +0900

    Create 1365_꼬인_전깃줄_G3.cpp

---
+++ Beakjoon/1_Gold/1365_꼬인_전깃줄_G3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+const int MAX = 100001;
+using namespace std;
+
+int n, ans = 0;
+int arr[MAX];
+vector<int> v;
+
+int main(){
+    cin >> n;
+
+    for(int i=1; i<=n; i++)
+        cin >> arr[i];
+
+    for(int i=1; i<=n; i++){
+        if(v.size() == 0 || v[v.size() -1] < arr[i])
+            v.push_back(arr[i]);
+        else{
+            int pos = lower_bound(v.begin(), v.end(), arr[i]) - v.begin();
+            v[pos] = arr[i];
+        }
+
+    }
+    
+    cout << n - v.size();
+}
